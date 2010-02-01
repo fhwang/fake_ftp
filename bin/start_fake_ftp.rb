afacf41e02eb0ebc64985ddb046c91d852d9a24f
Francis Hwang <francis@Francis-Hwangs-MacBook-Pro.local>
2010-02-01 16:35:08 -0500

    better start_fake_ftp.rb options

--- bin/start_fake_ftp.rb
+++ bin/start_fake_ftp.rb
@@ -1,6 +1,24 @@
 #!/usr/bin/env ruby
 
 require File.expand_path(File.dirname(__FILE__) + '/../lib/fake_ftp')
+require 'optparse'
 
-FakeFTP::Server.new :root_dir => '/'
+options = {:root_dir => '/', :port => 21}
+OptionParser.new do |opts|
+  opts.on(
+    "-r", "--root-dir ROOT_DIR",
+    "Use ROOT_DIR as the root directory of the FTP server"
+  ) do |root_dir|
+    options[:root_dir] = root_dir
+  end
+  
+  opts.on(
+    "-p", "--port FTP_PORT",
+    "Use FTP_PORT as the port number of the FTP server"
+  ) do |ftp_port|
+    options[:port] = ftp_port
+  end
+end.parse!
+
+FakeFTP::Server.new options
 while true; end
